034e2693c9207fed9ba4c79c9e9236bf0b17007f
korol <cx.korol@gmail.com>
2019-03-19 15:51:08 +0300

    Padawans task: advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  for (let i = 2; i <= base; i++) {
+    let y = 0;
+
+    if (base % i === 0) {
+
+      while (base % i === 0) {
+        y++;
+        base = base / i;
+      }
+
+      let x = 0;
+      let q = Math.floor(number / i);
+
+      while (q > 0) {
+        x += q;
+        q = Math.floor(q / i);
+      }
+
+      result = Math.floor(x / y);
+    }
+  }
+  return result;
 }
